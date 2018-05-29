5947e3d6c9aafb1813aee5e182e23504597487c6
mordenius <mordenius@gmail.com>
2018-05-29 16:16:00 +0300

    feature(node) docs

--- source/ts/node.ts
+++ source/ts/node.ts
@@ -1,17 +1,38 @@
 import { INode, NodeData } from "../../index";
 import { Emitter } from "./emitter";
 
+/**
+ * Node of data object structure
+ */
 export class Node extends Emitter implements INode {
+	/**
+	 * Value of Node data
+	 */
 	private data: NodeData;
 
+	/**
+	 * 
+	 * @param data Initialize data of Node
+	 */
 	constructor(data: NodeData) {
 		super();
 		this.data = data;
 	}
 
+	/**
+	 * Change value of Node data
+	 * @param data New data value
+	 */
 	public set(data: NodeData): void {
 		const prev = this.data;
 		this.data = data;
 		this.emit(this.data, prev);
+	}
+
+	/**
+	 * Get value of Node data
+	 */
+	public get(): NodeData {
+		return this.data;
 	}
 }
