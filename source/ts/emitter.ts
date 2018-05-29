565095f2618978324cca8bf74ad0397314351c39
mordenius <mordenius@gmail.com>
2018-05-29 14:09:51 +0300

    docs(emitter)

--- source/ts/emitter.ts
+++ source/ts/emitter.ts
@@ -1,29 +1,57 @@
 import { IEmitter, EmitterListener, NodeData } from "../../index";
 
+/**
+ * Callback listeners controller
+ */
 export class Emitter implements IEmitter {
-	private listeners: Map<number, EmitterListener> = new Map();
+	/**
+	 * Collections of listeners
+	 */
+	private readonly listeners: Map<number, EmitterListener> = new Map();
+	/**
+	 * Count of listeners
+	 */
 	private cnt: number = 0;
+	/**
+	 * Index, under which the next listener will be added to the collection
+	 */
 	private index: number = 0;
 
+	/**
+	 * Public getter for receive count of active listeners
+	 */
 	public get listenerCount(): number {
 		return this.cnt;
 	}
 
-	public subscribe(listener: EmitterListener): Function {
+	/**
+	 * Subscription of listener
+	 * @param listener Callback listener
+	 */
+	public subscribe(listener: EmitterListener): () => void {
 		const index: number = this.index;
 		this.listeners.set(index, listener);
 		this.cnt += 1;
 		this.index += 1;
 
-		return this.unsubscribe.bind(this, index);
+		return this.unsubscribe.bind(this, index) as () => void;
 	}
 
+	/**
+	 * Calling listeners with value transfer
+	 * @param data Current value
+	 * @param prevData Previously value
+	 */
 	public emit(data: NodeData, prevData: NodeData): void {
 		this.listeners.forEach((value: EmitterListener): void => {
 			value.call(null, data, prevData);
 		});
 	}
 
+	/**
+	 *  Unsubscribe of listener
+	 * @param index - Listener index in collection of listeners
+	 */
 	private unsubscribe(index: number): void {
 		this.listeners.delete(index);
 		this.cnt -= 1;
