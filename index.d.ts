edf9609692755739099c64c65e8c9f47ab184db8
Mordenius Norrogem <mordenius@gmail.com>
2018-06-13 00:46:14 +0300

    chore(index.d.ts)

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,24 @@
 
 export type CellData = string | number | boolean | any[] | Function;
 
-export interface ICell {
+export interface ICell extends IEmitter {
 	set(data: CellData): void;
 	get(): CellData;
 }
 
-export type NodeData = string | number | boolean | any[] | NodeData;
+export type NodeData = { [key: string]: CellData | NodeData };
 
 export interface INode extends IEmitter {
+	getChild(path: string | string[]): INode | ICell | undefined;
 	set(data: NodeData): void;
+	get(): NodeData;
+}
+
+export interface IBranch {
+	getChild(path: string[]): INode | ICell | undefined;
+	addNode(key: string, data: NodeData, listener: EmitterListener): void;
+	addCell(key: string, data: CellData, listener: EmitterListener): void;
+	removechild(key: string): void;
 }
 
 export type EmitterListener = (data: NodeData, previoslyData?: NodeData) => void;
@@ -17,11 +26,11 @@ export type EmitterListener = (data: NodeData, previoslyData?: NodeData) => void
 export interface IEmitter {
 	listenerCount: number;
 	subscribe(listener: EmitterListener): void;
-	emit(data: NodeData, prevData: NodeData): void;
+	emit(data: NodeData, prevData?: NodeData): void;
 }
 
 export type ProviderData = { [key: string ]: NodeData };
 
 export interface IProvider {
-
+	
 }
