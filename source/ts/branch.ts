30a4a55253bb18521f66f7e78a88911428c53621
Mordenius Norrogem <mordenius@gmail.com>
2018-06-24 00:40:10 +0300

    fix(branch) get first level child node

--- source/ts/branch.ts
+++ source/ts/branch.ts
@@ -11,55 +11,57 @@ import { Node } from "./node";
 
 export class Branch implements IBranch {
 	/**
-	 * 
+	 *
 	 */
 	private readonly nodes: Map<string, INode> = new Map();
 
 	/**
-	 * 
+	 *
 	 */
 	private readonly cells: Map<string, ICell> = new Map();
 
 	/**
-	 * 
-	 * @param path 
+	 *
+	 * @param path
 	 */
 	public getChild(path: string[]): INode | ICell | undefined {
 		let index = 0;
 		let child: INode | ICell | undefined =
 			this.nodes.get(path[index]) || this.cells.get(path[index]);
 
-		while (index < path.length && child !== undefined && child instanceof Node) {
-			++index;
+		while (
+			++index < path.length &&
+			child !== undefined &&
+			child instanceof Node
+		)
 			child = child.getChild(path[index]);
-		}
 
 		return child;
 	}
 
 	/**
-	 * 
-	 * @param key 
-	 * @param data 
-	 * @param listener 
+	 *
+	 * @param key
+	 * @param data
+	 * @param listener
 	 */
 	public addNode(key: string, data: NodeData, listener: EmitterListener): void {
 		this.nodes.set(key, new Node(data, listener));
 	}
 
 	/**
-	 * 
-	 * @param key 
-	 * @param data 
-	 * @param listener 
+	 *
+	 * @param key
+	 * @param data
+	 * @param listener
 	 */
 	public addCell(key: string, data: CellData, listener: EmitterListener): void {
 		this.cells.set(key, new Cell(data, listener));
 	}
 
 	/**
-	 * 
-	 * @param key 
+	 *
+	 * @param key
 	 */
 	public removechild(key: string): void {
 		this.cells.delete(key);
