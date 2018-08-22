0afe673295ad601f879b276449e61f72e3b9ca30
Mordenius Norrogem <mordenius@gmail.com>
2018-08-23 01:40:12 +0300

    fix(index.d.ts)

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,19 @@
+declare type EmitterListener = (data: NodeData, previoslyData?: NodeData) => void;
+declare type CellData = string | number | boolean | any[] | Function | undefined;
+declare type NodeData = { [key: string]: CellData | NodeData };
+declare type ProviderData = NodeData;
 
-export type CellData = string | number | boolean | any[] | Function | undefined;
+export interface IEmitter {
+	listenerCount: number;
+	subscribe(listener: EmitterListener): () => void;
+	emit(data: NodeData, prevData?: NodeData): void;
+}
 
 export interface ICell extends IEmitter {
 	name: string;
 	set(data: CellData): void;
 	get(): CellData;
 }
-
-export type NodeData = { [key: string]: CellData | NodeData };
 
 export interface INode extends IEmitter {
 	getChild(path: string | string[]): INode | ICell | undefined;
@@ -22,16 +28,31 @@ export interface IBranch {
 	removechild(key: string): void;
 }
 
-export type EmitterListener = (data: NodeData, previoslyData?: NodeData) => void;
+export interface IProvider extends INode {}
 
-export interface IEmitter {
+declare class Emitter implements IEmitter {
 	listenerCount: number;
 	subscribe(listener: EmitterListener): () => void;
 	emit(data: NodeData, prevData?: NodeData): void;
 }
 
-export type ProviderData = NodeData;
-
-export interface IProvider extends INode {
-	
+declare class Cell extends Emitter implements ICell {
+	name: string;
+	set(data: CellData): void;
+	get(): CellData;
 }
+
+declare class Node extends Emitter implements INode {
+	getChild(path: string | string[]): INode | ICell | undefined;
+	set(data: NodeData): void;
+	get(): NodeData;
+}
+
+declare class Branch implements IBranch {
+	getChild(path: string[]): INode | ICell | undefined;
+	addNode(key: string, data: NodeData, listener: EmitterListener): void;
+	addCell(key: string, data: CellData, listener: EmitterListener): void;
+	removechild(key: string): void;
+}
+
+declare class Provide extends Node implements IProvider {}
