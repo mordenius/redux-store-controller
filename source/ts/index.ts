ec352ac9a4e7dd2f374e2c69322182d40021c6af
Mordenius Norrogem <mordenius@gmail.com>
2018-08-26 15:51:38 +0300

    fix(provider) null cell value

--- source/ts/index.ts
+++ source/ts/index.ts
@@ -1,10 +1,4 @@
-import {
-	IProvider,
-	ProviderData,
-	CellData,
-	NodeData,
-	EmitterListener
-} from "../../index";
+import { IProvider, ProviderData, CellData, NodeData, EmitterListener } from "../../index";
 import { Node } from "./node";
 
 const defaultListener = (): void => {
@@ -13,14 +7,14 @@ const defaultListener = (): void => {
 
 export class Provider extends Node implements IProvider {
 	/**
-	 * 
+	 *
 	 */
 	public readonly origin: NodeData = {};
 
 	/**
-	 * 
-	 * @param data 
-	 * @param listener 
+	 *
+	 * @param data
+	 * @param listener
 	 */
 	constructor(data: ProviderData, listener?: EmitterListener) {
 		super(data, listener ? listener : defaultListener);
@@ -28,18 +22,15 @@ export class Provider extends Node implements IProvider {
 	}
 
 	/**
-	 * 
-	 * @param data 
+	 *
+	 * @param data
 	 */
 	private init(data: NodeData): NodeData {
 		const origin: NodeData = {};
 
 		for (const key in data) {
 			const value: NodeData | CellData = data[key];
-			origin[key] =
-				typeof value === "object" && !(value instanceof Array)
-					? this.init(value)
-					: value;
+			origin[key] = typeof value === "object" && !Array.isArray(value) && value ? this.init(value) : value;
 		}
 
 		return origin;
